479da7952ceb4d087f97b4babf19211eaa7cff83
Shourya <ss02184@surrey.ac.uk>
2020-03-02 22:10:11 +0000

    Caching now works on home, chat and contact page

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,10 +2,10 @@ class ChatController < ApplicationController
   before_action :authenticate_user!
   include ActionController::Live
 
+  caches_action :index, expires_in: 1.minute
 
   def index
-    @messages_count = Message.count
-    @messages = Message.order(created_at: :asc).last(5)
+    @messages = Message.order(created_at: :asc).all
   end
 
   def subscribe
