5fca0f8e861be2828773cff794b0c1698fdb9a28
Milen Georgiev <milendg99@gmail.com>
2020-03-15 00:58:49 +0000

    Added more logs in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  Rails.logger = Logger.new(STDOUT)
+  config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
