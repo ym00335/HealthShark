1b6143f46c8a29738b8486aaf8fe8fe66998e583
Milen Georgiev <milendg99@gmail.com>
2020-03-15 19:19:18 +0000

    Caching fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,11 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.cache_store = :memory_store
+  config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+  }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
