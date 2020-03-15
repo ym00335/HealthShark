5d357f51e78869d7c7a6dc1bc2835f26fcf7109d
Milen Georgiev <milendg99@gmail.com>
2020-03-15 18:26:13 +0000

    Revert "Added front-end caching" commit

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,6 @@ module HealthShark
     # Use the custom exception paths
     config.exceptions_app = self.routes
 
-    config.assets.precompile = ['.js', '.css', '*.css.erb']
-
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
