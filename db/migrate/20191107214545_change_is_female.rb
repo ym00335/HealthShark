d91723c43334b05d4bec9578e8daf5335cae7666
ym00335 <58860904+ym00335@users.noreply.github.com>
2020-04-09 01:03:14 +0100

    File removals for consistency

--- db/migrate/20191107214545_change_is_female.rb
+++
@@ -1,6 +0,0 @@
-class ChangeIsFemale < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :is_female
-    add_column :users, :is_female, :boolean
-  end
-end
--- db/migrate/20200331224635_add_detailsto_user.rb
+++
@@ -1,9 +0,0 @@
-class AddDetailstoUser < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :height, :integer
-    add_column :users, :weight, :integer
-    add_column :users, :diet, :string
-    add_column :users, :allergies, :text
-    
-  end
-end
